c535b1dc4aa21427397a13e4354e231075c10953
AndriiAV <Andrii.Andruschenko@gmail.com>
2017-08-11 14:20:23 +0300

    SOLID Dependency inversion. Interlayer between clients and low-level classes

---
+++ src/solid/DependencyInversion.java
@@ -0,0 +1,24 @@
+package solid;
+
+public class DependencyInversion {
+	public static void main(String[] args) {
+		
+	}
+}
+
+interface DAO{
+	void execute();
+}
+
+class Client{
+	DAO dataAccess = new DataAccess();
+	void doJob() {
+		dataAccess.execute();
+	};
+}
+
+class DataAccess  implements DAO{
+	public void execute() {
+		System.out.println("execute");
+	}
+}
